6ba8f5e560168324d9aab7044876bda440613e51
Conor <conor10@gmail.com>
2017-05-30 16:59:05 +1000

    Fixed cp issue

--- init.sh
+++ init.sh
@@ -13,7 +13,7 @@ cd ../erc20-rest-service
 for i in 1 2 3; do
   mkdir "../erc20-service${i}"
   cp build/libs/erc20-rest-service-0.1.0.jar "../erc20-service${i}"
-  cp "config/node${i}.yml" "../erc20-service${i}"
+  cp "../erc20-quorum-vm-example/config/node${i}.yml" "../erc20-service${i}"
 done
 
 ./erc20-services-start.sh
